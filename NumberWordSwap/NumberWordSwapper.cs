4309ba72fed461e906ef44f821f8f18e724047f6
revolt268 <revolt268@charter.net>
2020-12-01 11:14:54 -0600

    First Pass
    Cleaned up initial implmentation and posting first pass on git.

---
+++ NumberWordSwap/NumberWordSwapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberWordSwap
+{
+    public class NumberWordSwapper
+    {
+        // Find a way to get a subset of the results
+        //  Add a unit test to verify the 15 case
+        // Pass their own word pairs instead of Fizz Buzz
+        public static IList<string> ProcessValues(int upperLimit)
+        {
+            IList<string> results = new List<string>();
+            for (int index = 1; index <= upperLimit; index++)
+            {
+                string value = index.ToString();
+                if (index % 3 == 0 && index % 5 == 0)
+                {
+                    value = "FizzBuzz";
+                }
+                else if (index % 5 == 0)
+                {
+                    value = "Buzz";
+                }
+                else if (index % 3 == 0)
+                {
+                    value = "Fizz";
+                }
+                results.Add(value);
+            }
+            return results;
+        }
+    }
+}
---
+++ NumberWordSwapTestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberWordSwapTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pass in 3 parameters - 3 case Name, 5 case Name, and upper limit
+            // We want to return the results as we process them
+            IList<string> results = NumberWordSwap.NumberWordSwapper.ProcessValues(100);
+            //IList<string> results = NumberWordSwap.NumberWordSwapper.ProcessValues(int.MaxValue);
+            foreach (string result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
